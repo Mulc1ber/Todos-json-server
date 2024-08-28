c32fe17a084e38a7b726b8edb2eeb5d684b4cf54
Mikhail <dmo@m.com>
2024-08-28 16:12:55 +0300

    start

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,43 @@
-import './App.css';
+import {useState, useEffect} from 'react'
+import styles from './App.module.css';
 
 export const App = () => {
+    const [todos, setTodos] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        setIsLoading(true)
+
+       fetch('http://localhost:3003/todos')
+            .then((loadedData) => loadedData.json())
+            .then((loadedTodos) => {
+                setTodos(loadedTodos)
+            })
+            .finally(() => setIsLoading(false))
+    }, [])
+
     return (
-        <div className="App">
+        <div className={styles.App}>
             <h1>Todos JSON Server</h1>
+            <div>
+                <div>
+                    <input />
+                    <button>Добавить задачу</button>
+                    <button>Сортировать</button>
+                    <button>Поиск</button>
+                </div>
+                <div>
+                    {isLoading 
+                        ? <div className={styles.loader}></div>
+                        : todos.map(({id, title}) => (
+                            <div key={id} className={styles.todo}>
+                                <span>{title}</span>
+                                <button>Удалить</button>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     );
 };
